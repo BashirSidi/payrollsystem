9ab1fbf0debc61da60622a0c8d760015590a6363
BashirSidi <bashirsalisusidi@gmail.com>
2023-10-13 12:30:48 +0100

    initial configuration - mui - redux-toolkit

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import ThemeRegistry from '@/theme/ThemeRegistry'
+import { ReduxProvider } from './redux/provider'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Bauchi State Payroll System',
+  description: 'Bauchi State Payroll System',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <ThemeRegistry>
+        <body className={inter.className}>
+          <ReduxProvider>
+            {children}
+          </ReduxProvider>
+        </body>
+      </ThemeRegistry>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,29 @@
+"use client"
+import { Box, Container, Link, Typography } from '@mui/material'
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch } from './redux/store';
+
+
+export default function Home() {
+  const dispatch = useDispatch<AppDispatch>;
+  return (
+    <main>
+      <Container>
+        <Box>
+          <Typography
+            sx={{
+              mt: { xs: '10px', md: '60px' },
+              textAlign: 'center',
+              color: '#000',
+              fontSize:{ xs: '30px', md: '56px' },
+              fontWeight: { xs: 500, md: 700 },
+              lineHeight: '140%',
+            }}
+          >
+            Payroll System
+          </Typography>
+        </Box>
+        </Container>
+    </main>
+  )
+}
