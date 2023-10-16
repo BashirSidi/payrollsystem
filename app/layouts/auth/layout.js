31c7f228f3d89c27ca212970b6a61d649e050071
BashirSidi <bashirsalisusidi@gmail.com>
2023-10-16 17:35:00 +0100

    login page

---
+++ app/layouts/auth/layout.js
@@ -0,0 +1,86 @@
+import PropTypes from 'prop-types';
+import NextLink from 'next/link';
+import { Box, Typography, Unstable_Grid2 as Grid } from '@mui/material';
+import { Logo } from '@/app/components/logo';
+
+// TODO: Change subtitle text
+
+export const Layout = (props) => {
+  const { children } = props;
+
+  return (
+    <Box
+      component="main"
+      sx={{
+        background: '#fcf6eb',
+        display: 'flex',
+        flex: '1 1 auto'
+      }}
+    >
+      <Grid
+        container
+        sx={{ flex: '1 1 auto' }}
+      >
+        <Grid
+          xs={12}
+          lg={6}
+          sx={{
+            backgroundColor: 'background.paper',
+            display: 'flex',
+            flexDirection: 'column',
+            position: 'relative'
+          }}
+        >
+          <Box
+            component="header"
+            sx={{
+              left: 0,
+              p: 3,
+              position: 'fixed',
+              top: 0,
+              width: '100%'
+            }}
+          >
+            <Box
+              component={NextLink}
+              href="/"
+              sx={{
+                display: 'inline-flex',
+                height: 32,
+                width: 32
+              }}
+            >
+              {/* <Logo /> */}
+            </Box>
+          </Box>
+          {children}
+        </Grid>
+        <Grid
+          xs={12}
+          lg={6}
+          sx={{
+            alignItems: 'center',
+            background: 'radial-gradient(40% 40% at 40% 40%, #83585c  0%, #3D0C11 100%)',
+            color: 'white',
+            display: 'flex',
+            justifyContent: 'center',
+            '& img': {
+              maxWidth: '100%'
+            }
+          }}
+        >
+          <Box sx={{ p: 3, display: { xs: 'none', md: 'flex' } }}>
+            <img
+              alt=""
+              src="/images/auth-illustration.svg"
+            />
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+Layout.prototypes = {
+  children: PropTypes.node
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,161 @@
 "use client"
-import { Box, Container, Link, Typography } from '@mui/material'
-import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch } from './redux/store';
+import { useRouter } from 'next/navigation';
+import * as Yup from 'yup';
+import { useFormik } from 'formik';
+import NextLink from 'next/link';
+import Head from 'next/head';
+import {
+  Container,
+  Alert,
+  Box,
+  Button,
+  FormHelperText,
+  Link,
+  Stack,
+  Tab,
+  Tabs,
+  TextField,
+  Typography
+} from '@mui/material';
+import {Layout as AuthLayout} from '@/app/layouts/auth/layout'
+import { useState } from 'react';
 
 
-export default function Home() {
-  const dispatch = useDispatch<AppDispatch>;
+function Home() {
+  const router = useRouter();
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+      submit: null
+    },
+    validationSchema: Yup.object({
+      email: Yup
+        .string()
+        .email('Must be a valid email')
+        .max(255)
+        .required('Email is required'),
+      password: Yup
+        .string()
+        .max(255)
+        .required('Password is required')
+    }),
+    onSubmit: async (values, helpers) => {
+      try {
+        console.log('submitted', values)
+        // router.push('/');
+      } catch (err) {
+        console.error('Login failed.')
+      }
+    }
+  });
+  
   return (
-    <main>
-      <Container>
-        <Box>
-          <Typography
-            sx={{
-              mt: { xs: '10px', md: '60px' },
-              textAlign: 'center',
-              color: '#3D0C11',
-              fontSize:{ xs: '30px', md: '56px' },
-              fontWeight: { xs: 500, md: 700 },
-              lineHeight: '140%',
-            }}
-          >
-            Payroll System
-          </Typography>
+      <AuthLayout>
+       <Head>
+        <title>
+          Login
+        </title>
+      </Head>
+      <Box
+        sx={{
+          background: '#fcf6eb',
+          height: '100vh',
+          flex: '1 1 auto',
+          alignItems: 'center',
+          display: 'flex',
+          justifyContent: 'center'
+        }}
+      >
+        <Box
+          sx={{
+            maxWidth: 550,
+            background: '#fcf6eb',
+            px: 3,
+            py: '20px',
+            marginTop: '-150px',
+            width: '100%'
+          }}
+        >
+          <Box>
+            <Stack
+              spacing={1}
+              sx={{ mb: 3 }}
+            >
+              <Typography variant="h4">
+                Login
+              </Typography>
+            </Stack>
+              <form
+                noValidate
+                onSubmit={formik.handleSubmit}
+              >
+                <Stack spacing={3}>
+                  <TextField
+                    error={!!(formik.touched.email && formik.errors.email)}
+                    fullWidth
+                    helperText={formik.touched.email && formik.errors.email}
+                    label="Email Address"
+                    name="email"
+                    onBlur={formik.handleBlur}
+                    onChange={formik.handleChange}
+                    type="email"
+                    value={formik.values.email}
+                  />
+                  <TextField
+                    error={!!(formik.touched.password && formik.errors.password)}
+                    fullWidth
+                    helperText={formik.touched.password && formik.errors.password}
+                    label="Password"
+                    name="password"
+                    onBlur={formik.handleBlur}
+                    onChange={formik.handleChange}
+                    type="password"
+                    value={formik.values.password}
+                  />
+                </Stack>
+                {formik.errors.submit && (
+                  <Typography
+                    color="error"
+                    sx={{ mt: 3 }}
+                    variant="body2"
+                  >
+                    {formik.errors.submit}
+                  </Typography>
+                )}
+                <Button
+                  fullWidth
+                  size="large"
+                  sx={{
+                    mt: 3,
+                    background: '#3D0C11',
+                    color: '#fcf6eb',
+                    '&:hover': {
+                      backgroundColor: '#3D0C11',
+                      border: "none",
+                      boxShadow: "none",
+                      color: '#fcf6eb'
+                    },
+                    border: "none",
+                    boxShadow: "none"
+                   }}
+                  type="submit"
+                  variant="contained"
+                >
+                  Login
+                </Button>
+              </form>
+          </Box>
         </Box>
-        </Container>
-    </main>
+      </Box>
+          <Box sx={{display: { xs: 'none', md: 'flex' } }} className="ocean">
+            <Box className="wave"></Box>
+            <Box className="wave"></Box>
+          </Box>
+      </AuthLayout>
   )
 }
+
+export default Home;
