3c2ee14cdebb76f0f71a074620f0a7bb0b538c4c
BashirSidi <bashirsalisusidi@gmail.com>
2023-11-07 13:05:27 +0100

    add some changes

--- app/layouts/dashboard/config.js
+++ app/layouts/dashboard/config.js
@@ -1,11 +1,7 @@
 import ChartBarIcon from '@heroicons/react/24/solid/ChartBarIcon';
 import CogIcon from '@heroicons/react/24/solid/CogIcon';
-import LockClosedIcon from '@heroicons/react/24/solid/LockClosedIcon';
-import ShoppingBagIcon from '@heroicons/react/24/solid/ShoppingBagIcon';
-import UserIcon from '@heroicons/react/24/solid/UserIcon';
 import UserPlusIcon from '@heroicons/react/24/solid/UserPlusIcon';
 import UsersIcon from '@heroicons/react/24/solid/UsersIcon';
-import XCircleIcon from '@heroicons/react/24/solid/XCircleIcon';
 import { SvgIcon } from '@mui/material';
 
 export const items = [
@@ -28,44 +24,8 @@ export const items = [
     )
   },
   {
-    title: 'Benifits',
-    path: '/benefits',
-    icon: (
-      <SvgIcon fontSize="small">
-        <ShoppingBagIcon />
-      </SvgIcon>
-    )
-  },
-  {
-    title: 'Recruitement',
-    path: '/recruitment',
-    icon: (
-      <SvgIcon fontSize="small">
-        <UserIcon />
-      </SvgIcon>
-    )
-  },
-  {
-    title: 'Maintenance',
-    path: '/maintenance',
-    icon: (
-      <SvgIcon fontSize="small">
-        <CogIcon />
-      </SvgIcon>
-    )
-  },
-  {
-    title: 'Claim',
-    path: '/claim',
-    icon: (
-      <SvgIcon fontSize="small">
-        <LockClosedIcon />
-      </SvgIcon>
-    )
-  },
-  {
-    title: 'Register',
-    path: '/auth/register',
+    title: 'MDA',
+    path: '/mda',
     icon: (
       <SvgIcon fontSize="small">
         <UserPlusIcon />
@@ -73,12 +33,12 @@ export const items = [
     )
   },
   {
-    title: 'Error',
-    path: '/404',
+    title: 'Payrolls',
+    path: '/payrolls',
     icon: (
       <SvgIcon fontSize="small">
-        <XCircleIcon />
+        <CogIcon />
       </SvgIcon>
     )
-  }
+  },
 ];
