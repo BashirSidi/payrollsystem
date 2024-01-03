b6c8625a5bdf5039e728494c0e006c8b85fd5226
BashirSidi <bashirsalisusidi@gmail.com>
2024-01-03 01:07:19 +0100

    fix mda display list

--- app/mda/page.jsx
+++ app/mda/page.jsx
@@ -11,32 +11,29 @@ import { useDispatch, useSelector } from "react-redux";
 
 import { Layout as DashboardLayout } from '../layouts/dashboard/layout'
 import { DataGrid } from '@mui/x-data-grid';
-import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
-import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 import Head from 'next/head'
 import Loader from '../components/Loader'
 import { Scrollbar } from '../components/scrollbar';
-import { getAllMDAs } from '../redux/features/mdaSlice/thunk';
+import { fetchAllMda } from '../redux/features/mdaSlice/thunk';
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
   {
     field: 'title',
     headerName: 'Title',
     sortable: true,
     width: 600,
   },
-  {
-    field: 'action',
-    headerName: 'Action',
-    sortable: false,
-    width: 150,
-    renderCell: () =>
-      <Box>
-        <DeleteOutlinedIcon />
-        <EditOutlinedIcon />
-      </Box>
-  },
+  // {
+  //   field: 'action',
+  //   headerName: 'Action',
+  //   sortable: false,
+  //   width: 150,
+  //   renderCell: () =>
+  //     <Box>
+  //       <DeleteOutlinedIcon />
+  //       <EditOutlinedIcon />
+  //     </Box>
+  // },
 ];
 
 
@@ -45,9 +42,8 @@ const Page = () => {
   const mda = useSelector((state) => state.mda);
 
   useEffect(() => {
-    dispatch(
-        getAllMDAs({})
-    )
+    dispatch(fetchAllMda());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   if(mda.status === "loading"){
@@ -70,9 +66,9 @@ const Page = () => {
               <CardHeader title="Bauchi State MDAs" />
                 <Box>
                 <Scrollbar sx={{ flexGrow: 1 }}>
-                {mda?.mda?.data && 
+                {mda?.mdaList && 
                 <DataGrid
-                  rows={mda?.mda?.data}
+                  rows={mda?.mdaList}
                   columns={columns}
                   initialState={{
                     pagination: {
